a0e4a2b960a7812ccc40e49fd44de22d5e4f35bd
Palina Mitskevich <palina.mitskevich@gmail.com>
2022-10-19 14:09:07 +0200

    implements converter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { shownCurrency, url } from './constants';
 import './App.css';
 
 function App() {
+  const [currency, setCurrency] = useState({})
+  const [eur, setEur] = useState('')
+
+  useEffect(() => {
+      fetch(url)
+        .then((response) => response.json())
+        .then((currencies) => setCurrency(currencies))
+  }, [])
+
+  const handleInputChange = (event) => {
+    setEur(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <p className='title'>Currency converter</p>
+      <input type='number' min="0" className='input' placeholder='Enter EUR' value={eur} onChange={handleInputChange}/>
+        {Object.keys(currency).length && shownCurrency.map((cur) => {
+          return <div className='currency-container' key={cur}>
+            <p className='currency-name'>{cur}</p>
+            <p className='currency'>{(eur * (currency.rates[cur])).toFixed(2)}</p>
+          </div>
+        })}
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const url = 'https://api.exchangerate.host/latest'
+export const shownCurrency = ['USD', 'PLN', 'BYN', 'JPY']
